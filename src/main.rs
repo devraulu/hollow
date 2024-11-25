484a7a554398df1754b88054cf522a5112cc9ef5
Raúl Luis <soyrauluis@gmail.com>
2024-11-24 22:18:23 -0500

    async with tokio, reqwest and select
    Need to figure out a way around the 25 movies limitations, might have
    something to do with the headers or authorization that the website is
    using, maybe a web driver can help

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,64 @@
-use anyhow::{anyhow, Result};
-use reqwest::blocking::get;
-use scraper::{Html, Selector};
+use error_chain::error_chain;
+use reqwest::{
+    header::{HeaderMap, HeaderName, HeaderValue},
+    Client,
+};
+use select::document::Document;
+use select::predicate::{And, Child, Class, Name};
 
-fn main() {
-    let scraping_result = scrape_imdb_titles();
-    if let Err(e) = scraping_result {
-        eprintln!("Error: {e}");
+error_chain! {
+    foreign_links{
+        ReqError(reqwest::Error);
+IoError(std::io::Error);
     }
 }
 
-fn scrape_imdb_titles() -> Result<()> {
-    let url = "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&count=100";
+#[tokio::main]
+async fn main() -> Result<()> {
+    scrape_imdb_titles().await?;
 
-    let response = get(url).map_err(|e| anyhow!("Failed to fetch URL: {e}"))?;
+    Ok(())
+}
 
-    let text = response
-        .text()
-        .map_err(|e| anyhow!("Failed to read response text: {e}"))?;
+async fn scrape_imdb_titles() -> Result<()> {
+    let url = "https://www.imdb.com/search/title/";
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "Sec-Ch-Ua",
+        HeaderValue::from_static(
+            r##""Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99""##,
+        ),
+    );
+    let client = Client::builder().default_headers(headers).user_agent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36").https_only(true).build()?;
+    // ?groups=top_100&sort=user_rating,desc&count=100
+    let req = client
+        .get(url)
+        .query(&[
+            ("groups", "top_100"),
+            ("sort", "user_rating,desc"),
+            ("count", "100"),
+        ])
+        .build()?;
 
-    let document = Html::parse_document(&text);
+    let response = client.execute(req).await?.text().await?;
 
-    let title_selector = Selector::parse("a.ipc-title-link-wrapper>h3")
-        .map_err(|e| anyhow!("Failed to parse selector: {e}"))?;
+    // let text = response
+    //     .text()
+    //     .map_err(|e| anyhow!("Failed to read response text: {e}"))?;
 
-    let titles: Vec<_> = document
-        .select(&title_selector)
-        .map(|x| x.inner_html())
+    let document = Document::from(response.as_str());
+
+    let nodes: Vec<_> = document
+        // .find(And(Name("h3"), Class("ipc-title__text")))
+        .find(Child(
+            And(Name("a"), Class("ipc-title-link-wrapper")),
+            Name("h3"),
+        ))
         .collect();
 
-    println!("Displaying 100 of {} ", titles.len());
-    //
-    // titles
-    //     .iter()
-    //     .take(100)
-    //     .enumerate()
-    //     .for_each(|(index, title)| println!("{}. {}", index + 1, title));
+    let titles: Vec<_> = nodes.iter().map(|n| n.inner_html()).collect();
+
+    println!("nodes: {}, names: {}", nodes.len(), titles.len());
 
     for title in titles.iter().take(100) {
         println!("{title}");
